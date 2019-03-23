1a7192ccf2e6c83093aa7dc3957bfff6b3dda62a
Jonah Stiennon <jonahss@gmail.com>
2019-03-22 18:44:40 -0700

    edition 62

---
+++ javascript/Edition062_mitmproxy_ios.js
@@ -0,0 +1,62 @@
+let test = require('ava')
+let { remote } = require('webdriverio')
+let Mitmproxy = require('mitmproxy').default
+
+let proxy, driver
+let interceptedMessages = []
+
+// this function will get called every time the proxy intercepts a request
+let requestHandler = (message) => {
+  let req = message.request
+  console.log('************************************')
+  console.log('mitmproxy intercepted a request')
+  console.log(req.method)
+  console.log(req.rawUrl)
+  console.log(message.requestBody.toString())
+  console.log('************************************')
+  interceptedMessages.push(message)
+}
+
+test.before(async t => {
+  // start mitmproxy
+  proxy = await Mitmproxy.Create(requestHandler, [], true, true)
+
+  driver = await remote({
+    hostname: 'localhost',
+    port: 4723,
+    path: '/wd/hub',
+    capabilities: {
+      platformName: 'iOS',
+      platformVersion: '12.1',
+      deviceName: 'iPhone XS',
+      automationName: 'XCUITest',
+      app: 'https://github.com/cloudgrey-io/the-app/releases/download/v1.9.0/TheApp-v1.9.0.app.zip' // in order to download, you may need to install the mitmproxy certificate on your operating system first. Or download the app and replace this capability with the path to your app.
+    },
+    logLevel: 'silent'
+  })
+})
+
+test('getting the event for a day, via request to history.muffinlabs.com', async t => {
+  let pickerDemo = await driver.$('~Picker Demo')
+  await pickerDemo.click()
+  let button = await driver.$('~learnMore')
+  await button.click()
+  // wait for alert
+  await driver.waitUntil(function() {
+    return this.getAlertText().then(function(text) {
+        return text;
+    }, function() {
+        return false;
+    });
+  }, 4000)
+  await driver.dismissAlert()
+
+  t.true(interceptedMessages.length > 0)
+  t.true(interceptedMessages.some(m => m.request.rawUrl == 'https://history.muffinlabs.com/date/1/1'))
+})
+
+test.after.always(async t => {
+  t.log('shutting down')
+  await proxy.shutdown()
+  await driver.deleteSession()
+})
---
+++ javascript/Edition062_mitmproxy_ios_packet_injection.js
@@ -0,0 +1,67 @@
+let test = require('ava')
+let { remote } = require('webdriverio')
+let Mitmproxy = require('mitmproxy').default
+
+let proxy, driver
+let interceptedMessages = []
+
+// this is the response we will return from our proxy, instead of what the site usually returns
+let injectedResponse = {
+  date: "January 1",
+  url: "https://wikipedia.org/wiki/January_1",
+  data: {
+    Events: [
+      {
+        year: "2019",
+        text: "Tests Passed",
+      }
+    ]
+  }
+}
+
+let requestHandler = (message) => {
+  message.setResponseBody(Buffer.from(JSON.stringify(injectedResponse), 'utf8'))
+}
+
+test.before(async t => {
+  proxy = await Mitmproxy.Create(requestHandler, [], true, true)
+
+  driver = await remote({
+    hostname: 'localhost',
+    port: 4723,
+    path: '/wd/hub',
+    capabilities: {
+      platformName: 'iOS',
+      platformVersion: '12.1',
+      deviceName: 'iPhone XS',
+      automationName: 'XCUITest',
+      app: 'https://github.com/cloudgrey-io/the-app/releases/download/v1.9.0/TheApp-v1.9.0.app.zip' // in order to download, you may need to install the mitmproxy certificate on your operating system first. Or download the app and replace this capability with the path to your app.
+    },
+    logLevel: 'silent'
+  })
+})
+
+test(`insert our own event for a day, assert that it's displayed`, async t => {
+  let pickerDemo = await driver.$('~Picker Demo')
+  await pickerDemo.click()
+  let button = await driver.$('~learnMore')
+  await button.click()
+  // wait for alert
+  await driver.waitUntil(function() {
+    return this.getAlertText().then(function(text) {
+        return text;
+    }, function() {
+        return false;
+    });
+  }, 4000)
+  let alertText = await driver.getAlertText()
+  await driver.dismissAlert()
+
+  t.true(/Tests Passed/.test(alertText))
+})
+
+test.after.always(async t => {
+  t.log('shutting down')
+  await proxy.shutdown()
+  await driver.deleteSession()
+})
