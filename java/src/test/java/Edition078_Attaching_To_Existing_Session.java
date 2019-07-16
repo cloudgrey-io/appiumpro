bccd1f2def4bcae491957265651e8f47d90c5580
Jonah Stiennon <jonahss@gmail.com>
2019-07-16 14:15:23 -0700

    edition 78

---
+++ java/src/test/java/Edition078_Attaching_To_Existing_Session.java
@@ -0,0 +1,57 @@
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.ios.IOSDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+
+public class Edition078_Attaching_To_Existing_Session {
+    private String APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.9.0/TheApp-v1.9.0.app.zip";
+    private IOSDriver driver;
+    private static Boolean firstTest = true;
+
+    private String sessionId;
+
+    @Before
+    public void setUp() throws IOException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "iOS");
+        caps.setCapability("platformVersion", "12.2");
+        caps.setCapability("deviceName", "iPhone Xs");
+        caps.setCapability("automationName", "XCUITest");
+
+        caps.setCapability("app", APP);
+
+        driver = new IOSDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"), caps);
+        sessionId = driver.getSessionId().toString();
+    }
+
+    @After
+    public void tearDown() {
+        firstTest = false;
+        try {
+            driver.quit();
+        } catch (Exception ign) {}
+    }
+
+    @Test
+    public void testAttachingToSession() {
+        // This constructor has not yet been published, ongoing work is here: https://github.com/appium/java-client/pull/1181
+        IOSDriver newDriver = new AppiumDriver("http://localhost:4723/wd/hub" ,sessionId);
+
+        // new driver is equivalent to the original driver
+        String time = newDriver.getDeviceTime();
+        assertFalse(time.isEmpty());
+
+        // the original driver object still works fine
+        String laterTime = driver.getDeviceTime();
+        assertNotEquals(time, laterTime);
+    }
+}
