069ad79caff92b9fe782385f11ddcae91bc01edb
Jonah Stiennon <jonahss@gmail.com>
2019-08-14 00:18:44 -0500

    edition 82

---
+++ java/src/test/java/Edition082_Mjpeg_Video_Streaming_iOS.java
@@ -0,0 +1,116 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.AndroidStartScreenRecordingOptions;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.screenrecording.CanRecordScreen;
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import static junit.framework.TestCase.assertTrue;
+
+
+public class Edition082_Mjpeg_Video_Streaming_iOS {
+
+    private String IOS_APP = "/Users/jonahss/Workspace/TheApp-v1.9.0.app.zip";//https://github.com/cloudgrey-io/the-app/releases/download/v1.6.1/TheApp-v1.6.1.app.zip"; // in order to download, you may need to install the mitmproxy certificate on your operating system first. Or download the app and replace this capability with the path to your app.
+
+    private IOSDriver driver;
+
+    @After
+    public void Quit() {
+        driver.quit();
+    }
+
+    @Test
+    public void streamIosSimulatorVideo() throws IOException, URISyntaxException, InterruptedException, ExecutionException, TimeoutException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "iOS");
+        caps.setCapability("platformVersion", "12.4");
+        caps.setCapability("deviceName", "iPhone Xs");
+        caps.setCapability("automationName", "XCUITest");
+        caps.setCapability("app", IOS_APP);
+
+        caps.setCapability("newCommandTimeout", "0");
+
+        driver = new IOSDriver(new URL("http://0.0.0.0:4723/wd/hub"), caps);
+
+        driver.startRecordingScreen();
+
+        // now open a web browser on your computer pointed to http://localhost:9100 to watch a live stream of this test run.
+
+        for (int i = 0; i < 100; i++) {
+            WebElement list = driver.findElementByAccessibilityId("List Demo");
+            list.click();
+            Thread.sleep(1000);
+            WebElement back = driver.findElementByAccessibilityId("The App");
+            back.click();
+            Thread.sleep(1000);
+        }
+    }
+
+    @Test
+    public void timeScreenshotsWithDefaultBehavior() throws IOException, URISyntaxException, InterruptedException, ExecutionException, TimeoutException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "iOS");
+        caps.setCapability("platformVersion", "12.4");
+        caps.setCapability("deviceName", "iPhone Xs");
+        caps.setCapability("automationName", "XCUITest");
+        caps.setCapability("app", IOS_APP);
+
+        driver = new IOSDriver(new URL("http://0.0.0.0:4723/wd/hub"), caps);
+
+        driver.startRecordingScreen(); // this is unnecessary for this test run, but included here to make this test identical to the next test
+
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 100; i++) {
+            driver.getScreenshotAs(OutputType.FILE);
+        }
+        long endTime = System.nanoTime();
+
+        long msElapsed = (endTime - startTime) / 1000000;
+        System.out.println("100 screenshots normally: " + msElapsed + "ms. On average " + msElapsed/100 + "ms per screenshot");
+        // about 172ms per screenshot on my machine
+    }
+
+    @Test
+    public void timeScreenshotsWithMjpegScreenshotBehavior() throws IOException, URISyntaxException, InterruptedException, ExecutionException, TimeoutException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "iOS");
+        caps.setCapability("platformVersion", "12.4");
+        caps.setCapability("deviceName", "iPhone Xs");
+        caps.setCapability("automationName", "XCUITest");
+        caps.setCapability("app", IOS_APP);
+
+        driver = new IOSDriver(new URL("http://0.0.0.0:4723/wd/hub"), caps);
+
+        driver.startRecordingScreen(); // this is unnecessary for this test run, but included here to make this test identical to the next test
+
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 100; i++) {
+            driver.getScreenshotAs(OutputType.FILE);
+        }
+        long endTime = System.nanoTime();
+
+        long msElapsed = (endTime - startTime) / 1000000;
+        System.out.println("100 screenshots using mjpeg: " + msElapsed + "ms. On average " + msElapsed/100 + "ms per screenshot");
+        // about 436ms per screenshot on my machine
+    }
+}
