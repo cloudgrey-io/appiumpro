1392b4832e23782066daaff586fe0cd54d680b06
Jonathan Lipps <jlipps@gmail.com>
2019-11-26 11:02:56 -0800

    update 97

--- java/src/test/java/Edition097_Tracking_App_Launch_DeviceFarm.java
+++ java/src/test/java/Edition097_Tracking_App_Launch_DeviceFarm.java
@@ -1,7 +1,7 @@
 import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.serverevents.CommandEvent;
-import io.appium.java_client.serverevents.CustomEvent;
 import io.appium.java_client.serverevents.ServerEvents;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,6 +18,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Edition097_Tracking_App_Launch_DeviceFarm {
 
     private By loginScreen = MobileBy.AccessibilityId("Login Screen");
+    private static final String APP_PKG = "io.cloudgrey.the_app";
+    private static final String APP_ACT = ".MainActivity";
+    private static final String APP_WAIT = "com.reactnativenavigation.controllers.NavigationActivity";
+
 
     private AndroidDriver driver;
 
@@ -32,9 +36,9 @@ public class Edition097_Tracking_App_Launch_DeviceFarm {
         capabilities.setCapability("deviceName", "Android");
         capabilities.setCapability("automationName", "UiAutomator2");
         capabilities.setCapability("app", app);
-        capabilities.setCapability("appPackage", "io.cloudgrey.the_app");
-        capabilities.setCapability("appActivity", ".MainActivity");
-        capabilities.setCapability("appWaitActivity", "com.reactnativenavigation.controllers.NavigationActivity");
+        capabilities.setCapability("appPackage", APP_PKG);
+        capabilities.setCapability("appActivity", APP_ACT);
+        capabilities.setCapability("appWaitActivity", APP_WAIT);
         capabilities.setCapability("autoGrantPermissions", true);
         capabilities.setCapability("skipUnlock", true);
         capabilities.setCapability("ignoreUnimportantViews", true);
@@ -53,30 +57,33 @@ public class Edition097_Tracking_App_Launch_DeviceFarm {
 
     @Test
     public void testAppLaunch() throws Exception {
-        driver.launchApp();
         WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        // launch the app and wait for an element to be interactable
+        Activity act = new Activity(APP_PKG, APP_ACT);
+        act.setAppWaitActivity(APP_WAIT);
+        driver.startActivity(act);
         wait.until(ExpectedConditions.presenceOfElementLocated(loginScreen));
 
+
+        // pull out the events
         ServerEvents evts = driver.getEvents();
         List<CommandEvent> cmds = evts.getCommands();
-        Optional<CommandEvent> launchAppCmd = cmds.stream()
-            .filter((cmd) -> cmd.getName().equals("launchApp"))
+        Optional<CommandEvent> startActCmd = cmds.stream()
+            .filter((cmd) -> cmd.getName().equals("startActivity"))
             .findFirst();
         Optional<CommandEvent> findCmd = cmds.stream()
             .filter((cmd) -> cmd.getName().equals("findElement"))
             .findFirst();
 
-        if (!launchAppCmd.isPresent() || !findCmd.isPresent()) {
+        if (!startActCmd.isPresent() || !findCmd.isPresent()) {
             throw new Exception("Could not determine start or end time of app launch");
         }
 
-        long elapsedMs = findCmd.get().endTimestamp - launchAppCmd.get().startTimestamp;
+        long launchMs = startActCmd.get().endTimestamp - startActCmd.get().startTimestamp;
+        long interactMs = findCmd.get().endTimestamp - startActCmd.get().startTimestamp;
 
-        System.out.println("The app took <" + (elapsedMs / 1000.0) + "s to launch");
-
-        CustomEvent evt = new CustomEvent();
-        evt.setVendor("theApp");
-        evt.setEventName("mainScreenLoaded");
-        driver.logEvent(evt);
+        System.out.println("The app took total <" + (launchMs / 1000.0) + "s to launch " +
+                           "and total <" + (interactMs / 1000.0) + "s to become interactable");
     }
 }
