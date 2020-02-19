1025ee4ac68a7536bee10e0a19be36d7c0065e92
Jonathan Lipps <jlipps@gmail.com>
2020-02-18 23:56:16 -0800

    wip notifications

---
+++ java/src/test/java/Edition106_Android_Notifications.java
@@ -0,0 +1,46 @@
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class Edition106_Android_Notifications {
+    private String APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.10.0/TheApp-v1.10.0.apk";
+    private AndroidDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Android Emulator");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("app", APP);
+
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testNotifications() {
+        Map<String, Object> res = (Map<String, Object>)driver.executeScript("mobile: getNotifications");
+        List<Map<String, Object>> notifications = (List<Map<String, Object>>)res.get("statusBarNotifications");
+        for (Map<String, Object> notification : notifications) {
+            Map<String, String> innerNotification = (Map<String, String>)notification.get("notification");
+            System.out.println(innerNotification.get("bigTitle"));
+            System.out.println(innerNotification.get("bigText"));
+        }
+    }
+}
