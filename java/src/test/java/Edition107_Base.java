b49fb7571c76aa998d4598b15520599470ef0dde
Jonathan Lipps <jlipps@gmail.com>
2020-03-05 14:54:41 -0800

    add edition 107

--- java/src/test/java/Edition107_Base.java
+++ java/src/test/java/Edition107_Base.java
@@ -1,13 +1,24 @@
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
+import com.google.common.collect.ImmutableList;
+
+import org.junit.After;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
+import org.openqa.selenium.interactions.Pause;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.PointerInput.Kind;
+import org.openqa.selenium.interactions.PointerInput.MouseButton;
+import org.openqa.selenium.interactions.PointerInput.Origin;
+import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
 
 abstract public class Edition107_Base {
     protected AppiumDriver<MobileElement> driver;
@@ -15,9 +26,21 @@ abstract public class Edition107_Base {
     private By listView = MobileBy.AccessibilityId("List Demo");
     private By firstCloud = MobileBy.AccessibilityId("Altocumulus");
     private WebDriverWait wait;
+    private Dimension windowSize;
+
+    private static Duration SCROLL_DUR = Duration.ofMillis(1000);
+    private static double SCROLL_RATIO = 0.8;
+    private static int ANDROID_SCROLL_DIVISOR = 3;
 
     protected AppiumDriver<MobileElement> getDriver() {
         return driver;
+    }
+
+    @After
+    public void tearDown() {
+        try {
+            getDriver().quit();
+        } catch (Exception ign) {}
     }
 
     protected void navToList() {
@@ -26,7 +49,68 @@ abstract public class Edition107_Base {
         wait.until(ExpectedConditions.presenceOfElementLocated(firstCloud));
     }
 
-    protected void swipe(Point start, Point end, TimeUnit duration) {
+    private Dimension getWindowSize() {
+        if (windowSize == null) {
+            windowSize = getDriver().manage().window().getSize();
+        }
+        return windowSize;
+    }
+
+    protected void swipe(Point start, Point end, Duration duration) {
         AppiumDriver<MobileElement> d = getDriver();
+        boolean isAndroid = d instanceof AndroidDriver<?>;
+
+        PointerInput input = new PointerInput(Kind.TOUCH, "finger1");
+        Sequence swipe = new Sequence(input, 0);
+        swipe.addAction(input.createPointerMove(Duration.ZERO, Origin.viewport(), start.x, start.y));
+        swipe.addAction(input.createPointerDown(MouseButton.LEFT.asArg()));
+        if (isAndroid) {
+            duration = duration.dividedBy(ANDROID_SCROLL_DIVISOR);
+        } else {
+            swipe.addAction(new Pause(input, duration));
+            duration = Duration.ZERO;
+        }
+        swipe.addAction(input.createPointerMove(duration, Origin.viewport(), end.x, end.y));
+        d.perform(ImmutableList.of(swipe));
+    }
+
+    protected void swipe(double startXPct, double startYPct, double endXPct, double endYPct, Duration duration) {
+        Dimension size = getWindowSize();
+        Point start = new Point((int)(size.width * startXPct), (int)(size.height * startYPct));
+        Point end = new Point((int)(size.width * endXPct), (int)(size.height * endYPct));
+        swipe(start, end, duration);
+    }
+
+    protected void scroll(ScrollDirection dir, double distance) {
+        if (distance < 0 || distance > 1) {
+            throw new Error("Scroll distance must be between 0 and 1");
+        }
+        Dimension size = getWindowSize();
+        Point midPoint = new Point((int)(size.width * 0.5), (int)(size.height * 0.5));
+        int top = midPoint.y - (int)((size.height * distance) * 0.5);
+        int bottom = midPoint.y + (int)((size.height * distance) * 0.5);
+        int left = midPoint.x - (int)((size.width * distance) * 0.5);
+        int right = midPoint.x + (int)((size.width * distance) * 0.5);
+        if (dir == ScrollDirection.UP) {
+            swipe(new Point(midPoint.x, top), new Point(midPoint.x, bottom), SCROLL_DUR);
+        } else if (dir == ScrollDirection.DOWN) {
+            swipe(new Point(midPoint.x, bottom), new Point(midPoint.x, top), SCROLL_DUR);
+        } else if (dir == ScrollDirection.LEFT) {
+            swipe(new Point(left, midPoint.y), new Point(right, midPoint.y), SCROLL_DUR);
+        } else {
+            swipe(new Point(right, midPoint.y), new Point(left, midPoint.y), SCROLL_DUR);
+        }
+    }
+
+    protected void scroll(ScrollDirection dir) {
+        scroll(dir, SCROLL_RATIO);
+    }
+
+    protected void scroll() {
+        scroll(ScrollDirection.DOWN, SCROLL_RATIO);
+    }
+
+    public enum ScrollDirection {
+        UP, DOWN, LEFT, RIGHT
     }
 }
--- java/src/test/java/Edition107_Generic_Swipe_Actions_Android.java
+++ java/src/test/java/Edition107_Generic_Swipe_Actions_Android.java
@@ -7,21 +7,20 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.android.AndroidDriver;
 
-public class Edition107_Generic_Swipe_Actions_iOS extends Edition107_Base {
+public class Edition107_Generic_Swipe_Actions_Android extends Edition107_Base {
 
-    private String APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.10.0/TheApp-v1.10.0.app.zip";
-    private IOSDriver<MobileElement> driver;
+    private String APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.10.0/TheApp-v1.10.0.apk";
+    private AndroidDriver<MobileElement> driver;
 
     @Before
     public void setUp() throws IOException {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("platformName", "iOS");
-        caps.setCapability("platformVersion", "13.3");
-        caps.setCapability("deviceName", "iPhone 11");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("deviceName", "Android Emulator");
         caps.setCapability("app", APP);
-        driver = new IOSDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"), caps);
+        driver = new AndroidDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"), caps);
     }
 
     @Override
